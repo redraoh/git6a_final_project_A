1a375ba426ea15f2b285968e9b040db410aac908
Hoarder <kinskicom@gmail.com>
2024-04-18 15:05:35 +0900

    initial commit - 1st

---
+++ app/dbfactory.py
@@ -0,0 +1,27 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.models import board
+from app.settings import config
+from app.models import contact
+from app.models import member, product, cart, category, order
+
+# sqlite 사용시 check_same_thread 추가 - 쓰레드 사용 안함
+engine = create_engine(config.db_conn, echo=True,
+                       connect_args={'check_same_thread': False})
+
+Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+# 서버시작시 테이블 생성
+def db_startup():
+    contact.Base.metadata.create_all(engine)
+    category.Base.metadata.create_all(engine)
+    member.Base.metadata.create_all(engine)
+    product.Base.metadata.create_all(engine)
+    cart.Base.metadata.create_all(engine)
+    order.Base.metadata.create_all(engine)
+
+    board.Base.metadata.create_all(engine)
+
+# 테이블구조를 다시만들어주진않음
+
---
+++ main.py
@@ -0,0 +1,38 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from starlette.middleware.sessions import SessionMiddleware
+
+from app.dbfactory import db_startup
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield # 비동기 처리가 가능하게끔 yield
+    db_startup()
+
+app = FastAPI()
+
+# 세션처리를 미들웨어 설정
+app.add_middleware(SessionMiddleware, secret_key='050924duedate')
+
+# jinja2 설정
+templates = Jinja2Templates(directory='views/templates')
+app.mount('/static', StaticFiles(directory='views/static'), name='static')
+
+# 서버시작시 디비 생성
+@app.on_event('startup')
+async def on_startup():
+    db_startup()
+
+
+@app.get("/", response_class=HTMLResponse)
+async def index(req: Request):
+    return templates.TemplateResponse('index.html',{'request': req})    # 파일명과 넘길 데이터
+
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run('main:app', reload=True)
