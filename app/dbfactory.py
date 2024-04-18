5b80e81ed0e9b36cf787bbed22c10dedd52b300e
YeEul, Kwon <gruuriii@gmail.com>
2024-04-18 15:18:54 +0900

    add db model member coupon

--- app/dbfactory.py
+++ app/dbfactory.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.settings import config
-
+from app.models import member, coupon
 
 # sqlite 사용시 check_same_thread 추가 - 쓰레드 사용 안함
 engine = create_engine(config.db_conn, echo=True,
@@ -12,8 +12,8 @@ Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # 서버시작시 테이블 생성
 def db_startup():
-    return
-    #board.Base.metadata.create_all(engine)
+    member.Base.metadata.create_all(engine)
+    coupon.Base.metadata.create_all(engine)
 
 # 테이블구조를 다시만들어주진않음
 
