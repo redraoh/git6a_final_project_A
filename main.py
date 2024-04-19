2c509798c8f932a8417401224de125b610e68336
Jang <kinskicom@gmail.com>
2024-04-19 17:23:49 +0900

    minor bug updated

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from fastapi.staticfiles import StaticFiles
 from starlette.middleware.sessions import SessionMiddleware
 
 from app.dbfactory import db_startup
-
+from app.routes.car import car_router
 from app.routes.coupon import coupon_router
 from app.routes.member import member_router
 
@@ -28,9 +28,8 @@ app.mount("/static", StaticFiles(directory='views/static'), name='static')
 
 # 외부 route 파일 불러오기
 app.include_router(coupon_router)
+app.include_router(car_router)
 app.include_router(member_router)
-
-
 
 # 서버시작시 디비 생성
 @app.on_event('startup')
