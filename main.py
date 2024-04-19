9378d44bdc1e96bd14b4e3585269ebb2198a6f81
Jang <kinskicom@gmail.com>
2024-04-19 12:16:45 +0900

    login page created

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from starlette.middleware.sessions import SessionMiddleware
 from app.dbfactory import db_startup
 
 from app.routes.coupon import coupon_router
+from app.routes.login import login_router
 
 # @asynccontextmanager
 # async def lifespan(app: FastAPI):
@@ -26,6 +27,9 @@ templates = Jinja2Templates(directory='views/templates')
 
 # 외부 route 파일 불러오기
 app.include_router(coupon_router)
+app.include_router(login_router)
+
+
 
 # 서버시작시 디비 생성
 @app.on_event('startup')
