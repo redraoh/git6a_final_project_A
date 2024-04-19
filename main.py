0acfcd87db3068dfa8ecc2ed1d05fe3f4a6432c1
Seokhyeon Kim <tjrgusdlrk101@gmail.com>
2024-04-19 11:06:03 +0900

    웹 사이트가 열리도록 수정

--- main.py
+++ main.py
@@ -1,38 +1,28 @@
-from contextlib import asynccontextmanager
-
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse, RedirectResponse
-from fastapi.staticfiles import StaticFiles
+
+from fastapi.responses import HTMLResponse
 from starlette.middleware.sessions import SessionMiddleware
-
 from app.dbfactory import db_startup
-
-# @asynccontextmanager
-# async def lifespan(app: FastAPI):
-#     yield # 비동기 처리가 가능하게끔 yield
-#     db_startup()
+from app.routes.car import car_router
 
 app = FastAPI()
 
-# 세션처리를 미들웨어 설정
-app.add_middleware(SessionMiddleware, secret_key='050924duedate')
+app.add_middleware(SessionMiddleware, secret_key='20240216103735')
 
-# jinja2 설정
-# templates = Jinja2Templates(directory='views/templates')
-# app.mount('/static', StaticFiles(directory='views/static'), name='static')
+templates = Jinja2Templates(directory='views/templates')
 
-# 서버시작시 디비 생성
 @app.on_event('startup')
 async def on_startup():
     db_startup()
 
+app.include_router(car_router)
+
 
 @app.get("/", response_class=HTMLResponse)
 async def index(req: Request):
-    return templates.TemplateResponse('index.html',{'request': req})    # 파일명과 넘길 데이터
-
+    return templates.TemplateResponse('index.html', {'request': req})
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run('main:app', reload=True)
+    uvicorn.run('main:app',reload=True)
