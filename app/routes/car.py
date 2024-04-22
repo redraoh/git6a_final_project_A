cbf20aec979633f2241ae628dc1401a4baf0a748
Seokhyeon Kim <tjrgusdlrk101@gmail.com>
2024-04-22 17:23:35 +0900

    API 추가 - 차량 뒷자리 4자리 입력하면 그 차량의 데이터 베이스를 가져옴

--- app/routes/car.py
+++ app/routes/car.py
@@ -1,9 +1,12 @@
+from datetime import datetime
+
 from fastapi import APIRouter
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from fastapi.requests import Request
-
-
+from app.schemas.car import Car
+from app.services.car import CarService
+from fastapi.responses import JSONResponse
 car_router = APIRouter()
 
 
@@ -13,5 +16,15 @@ templates = Jinja2Templates(directory='views/templates')
 # 차량 데이터 조회
 
 @car_router.get('/cars', response_class=HTMLResponse)
-def cars(req: Request):
-    return templates.TemplateResponse('discount_car.html', {'request': req})
+def cars(request: Request):
+    return templates.TemplateResponse('discount_car.html', {'request': request, 'cars': cars})
+
+
+@car_router.get("/cars/{cno}", response_model=Car)
+def get_car_info_by_number(cno: str):
+    car_info = CarService.get_car_info_by_number(cno)
+    if car_info:
+        car_info.cno = str(car_info.cno)
+        return car_info
+    else:
+        return JSONResponse(content={"error": "Car not found"}, status_code=404)
